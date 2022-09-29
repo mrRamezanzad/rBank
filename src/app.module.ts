9ee66ca7367efbf7dd54c989ea87a9b6630f4dc0
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2022-09-29 23:45:04 +0330

    changes: - initialized project structure - added user service - added auth controller - added .env configuration service to app.module - added db configuration service to app.module

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { AuthController } from "./auth/auth.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      applicationName: process.env.APP_NAME,
+      type: "postgres",
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
+      logger: "debug",
+      entities: [],
+      synchronize: true,
+      retryAttempts: 20,
+      retryDelay: 3000,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController, AuthController],
+  providers: [AppService],
+})
+export class AppModule {}
